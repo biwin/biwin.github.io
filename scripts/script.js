f265da411fc4c558dd8fdebdad56bcd3ccebcdae
biwin <biwin007@gmail.com>
2014-09-18 23:13:29 +0530

    updates the scripts with window and standard height functions.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
 var viewport_height = $(document).height();
 
 $('.page').css('height',viewport_height);
+
+$(document).ready(function(){
+
+    var $windows = $('.page');
+
+    $windows.windows({
+        snapping: true,
+        snapSpeed: 3000,
+        snapInterval: 1100,
+        onScroll: function(s){},
+        onSnapComplete: function($el){},
+        onWindowEnter: function($el){}
+    });
+
+
+});
